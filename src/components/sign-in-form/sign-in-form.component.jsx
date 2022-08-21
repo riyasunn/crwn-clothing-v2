9ba1def7922262d788a095014d4f05a05d107307
Riya Sun <riya.sunn@gmail.com>
2022-08-22 11:19:18 +1200

    created authentication page component alongside sign in form

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -44,9 +44,25 @@ const SignInForm = () => {
      const response = await signInAuthUserWithEmailAndPassword (email, password);
 
      console.log(response);
+
       resetFormFields();
 
     }catch(error) {
+     switch (error.code) {
+      case 'auth/wrong-password':
+        alert('incorrect password for email');
+        break;
+      case 'auth/user-not-found':
+        alert('no user associated with this email');
+        break;
+      default:
+        console.log(error);
+     }
+      // if (error.code === "auth/wrong-password") {
+      //   alert("incorrect password for email");
+      // }else if(error.code === "auth/user-not-found") {
+      //   alert("no user associated with this email")
+      // }
      
     }
 
@@ -85,7 +101,7 @@ const SignInForm = () => {
 
        <div className="buttons-container">
         <Button type="submit">Sign In</Button>
-        <Button buttonType="google" onClick={signInWithGoogle}>Google sign in</Button>
+        <Button type="button" buttonType="google" onClick={signInWithGoogle}>Google sign in</Button>
        </div>
         
       </form>
