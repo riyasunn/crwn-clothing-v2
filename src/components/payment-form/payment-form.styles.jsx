95f7825cc23ad1f9ae8b0a9aff1446cca5758204
Riya Sun <riya.sunn@gmail.com>
2022-09-21 12:58:06 +1200

    styled payment buttom

--- src/components/payment-form/payment-form.styles.jsx
+++ src/components/payment-form/payment-form.styles.jsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import Button from '../button/button.component';
 
 export const PaymentFormContainer = styled.div`
     /* margin-top:200px; */
@@ -8,6 +9,8 @@ export const PaymentFormContainer = styled.div`
     align-items: center;
     justify-content: center;
     font-size: 25px; 
+    margin-bottom:70px;
+    text-align: center;
 
 `;
 
@@ -15,3 +18,14 @@ export const FormContainer = styled.form`
     height: 100px;
     min-width: 800px;
 `;
+
+export const PaymentButton = styled(Button)`
+    margin-left: auto;
+    margin-top:30px;
+    min-width: 200px;
+    height: 60px;
+    letter-spacing: 0.5px;
+    line-height: 60px;
+    padding: 0 50px 0 50px;
+    font-size: 25px;
+`
